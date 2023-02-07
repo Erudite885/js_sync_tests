098a8f22daf1dcec74fb404aa4ffbbb2d7257e9a
Erudite885 <erudite885@gmail.com>
2023-02-07 16:30:06 +0100

    test check for opposites

--- util.test.js
+++ util.test.js
@@ -3,9 +3,13 @@ const { generateText } = require("./util");
 test("should output name and age", () => {
   const text = generateText("Nat", 29);
   expect(text).toBe("Nat (29 years old)");
+  const text2 = generateText("Anna", 29);
+  expect(text2).toBe("Anna (19 years old)");
 });
 
 test("should output data-less text", () => {
   const text = generateText("", null);
   expect(text).toBe(" (null years old)");
+  const text2 = generateText();
+  expect(text).toBe("undefined (undefined years old)");
 });
