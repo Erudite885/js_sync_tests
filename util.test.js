32b448560363044b18e1980330a0d3ed4c1d43f0
Erudite885 <erudite885@gmail.com>
2023-02-07 22:25:52 +0100

    adding end to end tests

--- util.test.js
+++ util.test.js
@@ -1,3 +1,5 @@
+const puppeteer = require("puppeteer");
+
 const { generateText } = require("./util");
 
 test("should output name and age", () => {
@@ -18,3 +20,20 @@ test("should generate a valid text output", () => {
   const text = generateText("Nat", 29);
   expect(text).toBe("Nat (29 years old)");
 });
+
+test("should click around", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ["--window-size=1920,1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto("localhost:3000");
+  await page.click("input#name");
+  await page.type("input#name", "Anne");
+  await page.click("input#age");
+  await page.type("input#age", "26");
+  await page.click("#btnAddUser");
+  const finalText = await page.$eval(".user-item", (el) => el.textContent);
+  expect(finalText).toBe("Anne (26 years old)");
+}, 10000);
