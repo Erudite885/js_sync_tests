57a40a40af88aa8abcca7abafe64ae45f06a8602
Erudite885 <erudite885@gmail.com>
2023-02-07 16:26:31 +0100

    second test check for false positive

--- util.test.js
+++ util.test.js
@@ -4,3 +4,8 @@ test("should output name and age", () => {
   const text = generateText("Nat", 29);
   expect(text).toBe("Nat (29 years old)");
 });
+
+test("should output data-less text", () => {
+  const text = generateText("", null);
+  expect(text).toBe(" (null years old)");
+});
